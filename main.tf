5feebb3e33d275e046066135e91ae39e147c9f2b
Julija A <julijaand@yahoo.com>
2023-04-21 09:10:12 +0300

    egress added

--- main.tf
+++ main.tf
@@ -23,6 +23,13 @@ resource "aws_security_group" "final_assignment_team_four_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "final_assignment_team_four" {
