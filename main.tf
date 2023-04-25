ea6d1ff08335b2aa768f3bd62eac060c45591850
Julijaand <113669109+Julijaand@users.noreply.github.com>
2023-04-25 10:00:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,14 @@ resource "aws_security_group" "final_assignment_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }*/
-
+    
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
