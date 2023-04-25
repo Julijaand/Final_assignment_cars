c98ee147fe30ef5b4e6baf78c27d8f666bcbad02
Ignejoneigne <125908665+Ignejoneigne@users.noreply.github.com>
2023-04-25 13:09:10 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_key_pair" "julias_key" {
+resource "aws_key_pair" "mykeypair_test" {
   key_name   = var.key_name
   public_key = file(var.public_key_path)
 }
 
-resource "aws_security_group" "final_assignment_group_four_security_group" {
+resource "aws_security_group" "final_assignment_security_group" {
   name        = var.security_group_name
 
   ingress {
@@ -16,21 +16,22 @@ resource "aws_security_group" "final_assignment_group_four_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-/*
-  ingress {
+
+  /*ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }*/
-    
+  }
+  */
+
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -39,17 +40,17 @@ resource "aws_security_group" "final_assignment_group_four_security_group" {
   }
 }
 
-resource "aws_instance" "final_assignment_group_four" {
+resource "aws_instance" "final_assignment" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = aws_key_pair.julias_key.key_name
+  key_name      = aws_key_pair.mykeypair_test.key_name
   tags = {
-    Name = "final_assignment_group_four"
+    Name = "final_assignment"
   }
-  vpc_security_group_ids = [aws_security_group.final_assignment_group_four_security_group.id]
+  vpc_security_group_ids = [aws_security_group.final_assignment_security_group.id]
 
   provisioner "local-exec" {
-    command = "sleep 60 && ansible-playbook -i '${aws_instance.final_assignment_group_four.public_ip},' -e ip_address=${aws_instance.final_assignment_group_four.public_ip} playbook.yml --user ${var.aws_instance_user_id} --private-key ${var.private_key_path} --vault-password-file vault_password.txt"
+    command = "sleep 80 && ansible-playbook -i '${aws_instance.final_assignment.public_ip},' -e ip_address=${aws_instance.final_assignment.public_ip} final_playbook.yml --user ${var.aws_instance_user_id} --private-key ${var.private_key_path} --vault-password-file vault_password.txt"
   }
 
 }
